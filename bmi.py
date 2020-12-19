e355487bcbea90d770de3b40e559c6fe73038a1a
elysekaaden <70551338+elysekaaden@users.noreply.github.com>
2020-12-19 20:11:34 +1100

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -2,12 +2,11 @@ print('\nBMI weight guide \n')
 weight = float(input('Please enter your weight(kg): '))
 height = float(input('Please enter your height(meter): '))
 bmi = weight / (height * height)
-print(bmi)
 if bmi < 18.5 :
-    print('Your BMI number is ', bmi, 'Underweight.')
+    print('Your BMI number is ', bmi, '- Underweight.')
 elif bmi >= 25 and bmi < 29.9 :
-    print('Your BMI number is ', bmi, 'Overweight')  
+    print('Your BMI number is ', bmi, '- Overweight.')  
 elif bmi >= 30 :
-    print('Your BMI number is ', bmi, 'Obese')    
+    print('Your BMI number is ', bmi, '- Obese.')    
 else:
-    print('Your BMI number is ', bmi, 'Healthy')
+    print('Your BMI number is ', bmi, '- Healthy.')
